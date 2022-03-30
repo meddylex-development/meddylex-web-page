034c35c5e10a3b03b5a0a07d99e8a3faae9b6da7
Juank001001 <juan.mendez@proyectatsp.com>
2022-03-30 11:14:47 -0500

    Agrupar servicios

--- src/app/slt/pages/home/home.component.ts
+++ src/app/slt/pages/home/home.component.ts
@@ -22,10 +22,79 @@ export class HomeComponent {
   }
   
 
+  dropdown_classV:string = "valores-dropdown desactivado";
+  dropdown_classP:string = "valores-dropdown desactivado";
 
+  dropDownVState:boolean = false;
+  dropDownPState:boolean = false;
+  dropDownTitle : string = "";
+  dropDownContent : string = "";
 
+  valores: any = 
+    { 
+      
+      Emisión_incapacidad:{
+        titulo: "Emisión incapacidad",
+        contenido : "Entendemos que el interés común prevalece sobre el particular, nos adherimos a los principios de desarrollo sostenible, estamos dispuestos a colaborar con los demás por el beneficio común, y obramos en consecuencia"
+      },
+
+      Concepto_rehabilitacion:{
+        titulo : "Concepto rehabilitación",
+        contenido : "Nos situamos en el lugar del otro, lo escuchamos, lo valoramos y somos receptivos a sus necesidades, haciendo de la armonía y confianza las bases fundamentales de nuestras relaciones"
+      },
+
+      Calificacion_origen:{
+        titulo: "Calificación de origen",
+        contenido: "Actuamos con honestidad, lealtad y franqueza. Hacemos lo que decimos y cumplimos nuestros compromisos"
+      },
+
+      Calificacion_PCL:{
+        titulo: "Calificación de PCL",
+        contenido: "Somos autoexigentes, ordenados y perseverantes. Agregamos un esfuerzo extra para convertir los retos y metas en logros y alcanzar la excelencia como práctica habitual"
+      },
+
+      Auditoria_Incapacidades_Prolongadas:{
+        titulo: "Auditoria Incapacidades Prolongadas",
+        contenido: "Somos autoexigentes, ordenados y perseverantes. Agregamos un esfuerzo extra para convertir los retos y metas en logros y alcanzar la excelencia como práctica habitual"
+      },
+
+      Identificacion_presuntos_Abusos_derecho:{
+        titulo: "Identificación presuntos Abusos del derecho",
+        contenido: "Somos autoexigentes, ordenados y perseverantes. Agregamos un esfuerzo extra para convertir los retos y metas en logros y alcanzar la excelencia como práctica habitual"
+      }
+
+    }
+
+  activarDropDownV(codigo : string){
+
+    if(!this.dropDownVState){
+
+      this.dropdown_classV = "valores-dropdown";
+      this.dropdown_classP += " desactivado";
+
+      const opcion = this.valores[codigo];
+      const {titulo,contenido} = opcion;
+
+      this.dropDownTitle = titulo;
+      this.dropDownContent = contenido;
+
+      this.dropDownVState = true;
+      this.dropDownPState = false;
+      
+    }else{
+
+      this.dropdown_classP += " desactivado";
+      const opcion = this.valores[codigo];
+      const {titulo,contenido} = opcion;
+
+      this.dropDownPState = false;
+
+      this.dropDownTitle = titulo;
+      this.dropDownContent = contenido;
+      
+    }
+  }
 
   constructor() { }
-
   
 }
