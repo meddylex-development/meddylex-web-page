c86bc1e4682016824a804a3db580df4f6591d626
JOSE EDUARDO QUIÑONES <joseeduardoquinones@gmail.com>
2022-04-06 09:48:19 -0500

    Se modifico los iconos de los componentes y detalle de los servicios

--- src/app/slt/pages/home/home.component.ts
+++ src/app/slt/pages/home/home.component.ts
@@ -39,18 +39,18 @@ export class HomeComponent {
       },
 
       Concepto_rehabilitacion:{
-        titulo : "Concepto rehabilitación",
-        contenido : "Nos situamos en el lugar del otro, lo escuchamos, lo valoramos y somos receptivos a sus necesidades, haciendo de la armonía y confianza las bases fundamentales de nuestras relaciones"
+        titulo : "Concepto rehabilitación y reintegro laboral",
+        contenido : "Suministramos asesoría profesional especializada en la gestión de seguimiento del proceso de rehabilitación y retorno a la actividad laboral de trabajadores afectados por una enfermedad general, una enfermedad profesional o un accidente de trabajo."
       },
 
       Calificacion_origen:{
         titulo: "Calificación de origen",
-        contenido: "Actuamos con honestidad, lealtad y franqueza. Hacemos lo que decimos y cumplimos nuestros compromisos"
+        contenido: "Contamos con peritos especializados en identificar la relación de causalidad de la enfermedad o el accidente, cuyo resultado determinará la entidad responsable del reconocimiento de las prestaciones asistenciales y económicas."
       },
 
       Calificacion_PCL:{
         titulo: "Calificación de PCL",
-        contenido: "Somos autoexigentes, ordenados y perseverantes. Agregamos un esfuerzo extra para convertir los retos y metas en logros y alcanzar la excelencia como práctica habitual"
+        contenido: "Nuestros peritos son expertos en establecer el porcentaje de afectación del conjunto de habilidades, destrezas, aptitudes y/o potencialidades de orden físico, mental y social que posibilitan desempeñarse en un trabajo, el grado de invalidez y la fecha de su estructuración, como consecuencia de un accidente o una enfermedad."
       },
 
       Auditoria_Incapacidades_Prolongadas:{
